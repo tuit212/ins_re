a7e9a732a0746de7a72f0d78d1f82560932c1472
tuit212 <asadbekanvarov9808@example.com>
2023-03-10 21:10:19 +0500

    profile change

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Pages.scss'
 import Navbar from '../../components/Navbar/Navbar'
+import { NavLink } from 'react-router-dom'
 
 function Profile() {
     return (
@@ -9,7 +10,40 @@ function Profile() {
                 <Navbar/>
             </div>
             <div className='right'>
-                <h1>profile assalom</h1>
+                <div className="profile">
+                    <div className="profile__containr">
+                        <div className="profile">
+                            <div className="profile__left">
+                                salom
+                            </div>
+                            <div className="profile__right">
+                                <div className="profile__right-top">
+                                    <h3>asadbek.2305</h3>
+                                    <NavLink>Edit Profile</NavLink>
+                                    <NavLink>Add tools</NavLink>
+                                    <NavLink>
+                                        <i class="fa-solid fa-gear"></i>
+                                    </NavLink>
+                                </div>
+                                <div className="profile__right-center">
+                                    <div>
+                                        <span>2</span>posts
+                                    </div>
+                                    <div>
+                                        <span>985</span>followers
+                                    </div>
+                                    <div>
+                                        <span>2</span>following
+                                    </div>
+                                </div>
+                                <div className="profile__right-bottom">
+                                    <h5>Asadbek Asadbek</h5>
+                                    <NavLink>tuit_212</NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
